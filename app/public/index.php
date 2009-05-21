e06283439b7af6dfd6ff0a6b8960eb0b1f8f91ea
joanhey <joanhey@kumbiaphp.com>
2009-05-21 06:15:25 +0200

    Cambio de licencia del index.php en public

--- app/public/index.php
+++ app/public/index.php
@@ -1,22 +1,23 @@
 <?php
 /**
- * Kumbia PHP Framework
- * PHP version 5
+ * KumbiaPHP web & app Framework
+ *
  * LICENSE
  *
- * This source file is subject to the GNU/GPL that is bundled
- * with this package in the file docs/LICENSE.txt.
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
  * It is also available through the world-wide-web at this URL:
- * http://www.kumbiaphp.com/
+ * http://wiki.kumbiaphp.com/Licencia
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
- * to kumbia@kumbiaphp.com so we can send you a copy immediately.
+ * to license@kumbiaphp.com so we can send you a copy immediately.
+ *
+ * Clase para manejar las peticiones de KumbiaPHP Framework
  * 
- * @author    Andres Felipe Gutierrez <andresfelipe@vagoogle.net>
- * @copyright 2007-2008 Emilio Rafael Silveira Tovar <emilio.rst at gmail.com>
- * @copyright 2007-2008 Deivinson Jose Tejeda Brito <deivinsontejeda at gmail.com>
- * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt GNU/GPL
- * @version   SVN:$id
+ * @category   Kumbia
+ * @package    Dispatcher 
+ * @copyright  Copyright (c) 2005-2009 Kumbia Team (http://www.kumbiaphp.com)
+ * @license    http://wiki.kumbiaphp.com/Licencia     New BSD License
  */
 
 /**
