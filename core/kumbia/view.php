a0d4b6bad9051df914ae29ae709a172baeb8be0f
joanhey <joanhey@kumbiaphp.com>
2009-10-26 16:30:10 +0100

    Añadido a la clase View los atajos h y eh. Ya que asi no colisianan. Además son útiles, incluso sin los helpers.

--- core/kumbia/view.php
+++ core/kumbia/view.php
@@ -186,3 +186,27 @@ class View
         }
     }
 }
+/**
+ * Atajo para htmlspecialchars, por defecto toma el charset de la
+ * aplicacion
+ *
+ * @param string $s
+ * @param string $charset
+ * @return string
+ **/
+function h($s, $charset = APP_CHARSET) {
+    
+    return htmlspecialchars($s, ENT_QUOTES, $charset);
+}
+/**
+ * Atajo para echo + htmlspecialchars, por defecto toma el charset de la
+ * aplicacion
+ *
+ * @param string $s
+ * @param string $charset
+ * @return string
+ **/
+function eh($s, $charset = APP_CHARSET) {
+    
+    echo htmlspecialchars($s, ENT_QUOTES, $charset);
+}
