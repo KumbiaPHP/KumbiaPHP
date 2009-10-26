8a992611154aa781d9d4c18d92a8e381bd419b22
joanhey <joanhey@kumbiaphp.com>
2009-10-26 16:28:19 +0100

    Eliminado los atajos h y eh de los helpers tag y tags. Ya que colisionaban al cargar ambos helpers.

--- core/extensions/helpers/tag.php
+++ core/extensions/helpers/tag.php
@@ -108,29 +108,3 @@ class Tag
         echo $code;
     }
 }
-
-
-/**
- * Atajo para htmlspecialchars, por defecto toma el charset de la
- * aplicacion
- *
- * @param string $s
- * @param string $charset
- * @return string
- **/
-function h($s, $charset = APP_CHARSET) {
-    
-    return htmlspecialchars($s, ENT_QUOTES, $charset);
-}
-/**
- * Atajo para echo + htmlspecialchars, por defecto toma el charset de la
- * aplicacion
- *
- * @param string $s
- * @param string $charset
- * @return string
- **/
-function eh($s, $charset = APP_CHARSET) {
-    
-    echo htmlspecialchars($s, ENT_QUOTES, $charset);
-}
--- core/extensions/helpers/tags.php
+++ core/extensions/helpers/tags.php
@@ -1846,27 +1846,3 @@ function stylesheet_link_tags()
         }
     }
 }
-/**
- * Atajo para htmlspecialchars, por defecto toma el charset de la
- * aplicacion
- *
- * @param string $s
- * @param string $charset
- * @return string
- **/
-function h($s, $charset = APP_CHARSET) {
-    
-    return htmlspecialchars($s, ENT_QUOTES, $charset);
-}
-/**
- * Atajo para echo + htmlspecialchars, por defecto toma el charset de la
- * aplicacion
- *
- * @param string $s
- * @param string $charset
- * @return string
- **/
-function eh($s, $charset = APP_CHARSET) {
-    
-    echo htmlspecialchars($s, ENT_QUOTES, $charset);
-}
