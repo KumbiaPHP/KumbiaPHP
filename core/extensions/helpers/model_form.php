7db4466f1c848ff170b41797100d7253ccd375d9
joanhey <joanhey@kumbiaphp.com>
2009-11-05 13:32:29 +0100

    Añadido el phpdoc al helper ModelForm.

--- core/extensions/helpers/model_form.php
+++ core/extensions/helpers/model_form.php
@@ -1,8 +1,31 @@
 <?php
-
+/**
+ * KumbiaPHP web & app Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://wiki.kumbiaphp.com/Licencia
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@kumbiaphp.com so we can send you a copy immediately.
+ *
+ * Helper para crear Forms de un modelo automáticamente
+ * 
+ * @category   KumbiaPHP
+ * @package    Helpers 
+ * @copyright  Copyright (c) 2005-2009 KumbiaPHP Team (http://www.kumbiaphp.com)
+ * @license    http://wiki.kumbiaphp.com/Licencia     New BSD License
+ */
 
 class ModelForm {
-
+	/**
+     * Genera un form de un modelo (objeto) automáticamente
+     *
+     * @var object 
+     **/
 	public static function create($model, $action = null) {
 	
 		$model_name = Util::smallcase(get_class($model));
