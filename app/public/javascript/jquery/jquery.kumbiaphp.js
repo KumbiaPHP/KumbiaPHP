45c935750ceeff06b563a3b5fac83c0a99b3076b
Emilio Silveira <emilio.rst@gmail.com>
2010-02-24 09:23:51 -0430

    Helpers
    Js::calendar
    Js::updaterSelect
    Js::updaterDbSelect
    
    Ahora utilizan los "data-" de HTML 5
    
    Tambien corregido el partial de carga para jquery

--- app/public/javascript/jquery/jquery.kumbiaphp.js
+++ app/public/javascript/jquery/jquery.kumbiaphp.js
@@ -21,63 +21,68 @@
 	/**
 	 * Objeto KumbiaPHP
 	 *
-	 **/
+	 */
 	$.KumbiaPHP = {
 		plugin: [],
+        
 		/**
 		 * Muestra mensaje de confirmacion
 		 *
 		 * @param Object event
-		 **/
+		 */
 		cConfirm: function(event) {
 			if(!confirm(this.title)) {
 				event.preventDefault();
 			}
 		},
+        
 		/**
 		 * Aplica un efecto a un elemento
 		 *
 		 * @param String fx
-		 **/
+		 */
 		cFx: function(fx) {
 			return function(event) {
 				event.preventDefault();
 				(($(this.rel))[fx])();
 			}
 		},
+        
 		/**
 		 * Carga con AJAX
 		 *
 		 * @param Object event
-		 **/
+		 */
 		cRemote: function(event) {
 			event.preventDefault();
 			$(this.rel).load(this.href);
 		},
+        
 		/**
 		 * Carga con AJAX y Confirmacion
 		 *
 		 * @param Object event
-		 **/
+		 */
 		cRemoteConfirm: function(event) {
 			event.preventDefault();
 			if(confirm(this.title)) {
 				$(this.rel).load(this.href);
 			}
 		},
+        
 		/**
 		 * Metodo para utilizar con map de jQuery, el cual genera calendarios jsCalendar
 		 *
-		 **/
+		 */
 		calendar: function() {
-			var tigger = $('#' + this.id + '_tigger').get(0);
+			var tigger = document.getElementById(this.id + '.tigger');
 			Calendar.setup({ inputField: this.id, ifFormat: tigger.alt, daFormat: tigger.alt, button: tigger.id});
 		},
 
 		/**
 		 * Enviar formularios de manera asincronica, via POST
 		 * Y los carga en un contenedor
-		 **/
+		 */
 		cFRemote: function(e){
 			e.preventDefault();
 			self = $(this);
@@ -98,16 +103,15 @@
 		 * Carga con AJAX al cambiar select
 		 *
 		 * @param Object event
-		 **/
+		 */
 		cUpdaterSelect: function(event) {
-			$('#' + $.KumbiaPHP.metadata[this.id].update).load($.KumbiaPHP.metadata[this.id].action + this.value);
+			$('#' + $(this).attr('data-update')).load($(this).attr('data-action') + this.value);
 		},
-
 
 		/**
 		 * Enlaza a las clases por defecto
 		 *
-		 **/
+		 */
 		bind : function() {
 			$("a.js-confirm").live('click', this.cConfirm);
 			$("a.js-remote").live('click', this.cRemote);
@@ -121,48 +125,52 @@
 			$("div.js-remote").ajaxComplete(function (event, XMLHttpRequest, ajaxOptions) {
 				$.KumbiaPHP.bindNoLive(this);
 			});
+            
 			// enlaza los que no funcionan con live
 			this.bindNoLive(document);
 		},
-			/**
-			* Aquellos que no se enlazan automaticamente con live
-			*
-			* @param DomObject parent
-			**/
-			bindNoLive : function(parent) {
-				// lista desplegable que actualiza con ajax
-				$("select.js-remote", parent).bind('change', this.cUpdaterSelect);
-				// calendario
-				$("input.js-calendar", parent).map(this.calendar);
-				// formulario ajax
-				$("form.js-remote", parent).bind('submit', this.cFRemote);
-			},
-			/* Implementa la autocarga de plugins, estos deben seguir
-			 * una convención para que pueda funcionar correctamente
-			 */
-			autoload: function(){
-				var elem = $("[class*='jp-']");
-				$.each(elem, function(i, val){
-					var self = $(this); //apunta al elemento con clase jp-*
-					var classes = self.attr('class').split(' ');
-					for (i in classes){
-						if(classes[i].substr(0, 3) == 'jp-'){
-							if(jQuery.inArray(classes[i].substr(3),$.KumbiaPHP.plugin) != -1)
-								continue;
-							$.KumbiaPHP.plugin.push(classes[i].substr(3))
-						}
-					}
-				});
-				var head = $('head');
-				for(i in $.KumbiaPHP.plugin){
-					jQuery.ajaxSetup({ cache: true});
-					head.append('<link href="css/'+$.KumbiaPHP.plugin[i]+'.css" type="text/css" rel="stylesheet"/>');
-					jQuery.getScript('javascript/jquery/jquery.'+$.KumbiaPHP.plugin[i]+'.js', function(data, text){
-
-					});
-			}
+        
+        /**
+         * Aquellos que no se enlazan automaticamente con live
+         *
+         * @param DomObject parent
+         */
+        bindNoLive : function(parent) {
+            // lista desplegable que actualiza con ajax
+            $("select.js-remote", parent).bind('change', this.cUpdaterSelect);
+            // calendario
+            $("input.js-calendar", parent).map(this.calendar);
+            // formulario ajax
+            $("form.js-remote", parent).bind('submit', this.cFRemote);
+        },
+        
+        /**
+         * Implementa la autocarga de plugins, estos deben seguir
+         * una convención para que pueda funcionar correctamente
+         */
+        autoload: function(){
+            var elem = $("[class*='jp-']");
+            $.each(elem, function(i, val){
+                var self = $(this); //apunta al elemento con clase jp-*
+                var classes = self.attr('class').split(' ');
+                for (i in classes){
+                    if(classes[i].substr(0, 3) == 'jp-'){
+                        if(jQuery.inArray(classes[i].substr(3),$.KumbiaPHP.plugin) != -1)
+                            continue;
+                        $.KumbiaPHP.plugin.push(classes[i].substr(3))
+                    }
+                }
+            });
+            var head = $('head');
+            for(i in $.KumbiaPHP.plugin){
+                jQuery.ajaxSetup({ cache: true});
+                head.append('<link href="css/'+$.KumbiaPHP.plugin[i]+'.css" type="text/css" rel="stylesheet"/>');
+                jQuery.getScript('javascript/jquery/jquery.'+$.KumbiaPHP.plugin[i]+'.js', function(data, text){
+                });
+            }
 		}
 	}
+    
 	// Enlaza a las clases por defecto
 	$(function(){ $.KumbiaPHP.bind(); $.KumbiaPHP.autoload()});
 })(jQuery);
--- core/extensions/helpers/js.php
+++ core/extensions/helpers/js.php
@@ -19,12 +19,6 @@
  * @copyright  Copyright (c) 2005-2009 KumbiaPHP Team (http://www.kumbiaphp.com)
  * @license    http://wiki.kumbiaphp.com/Licencia     New BSD License
  */
- 
-/**
- * @see Tag
- **/
-
- 
 class Js
 {
     /**
@@ -94,129 +88,67 @@ class Js
     /**
      * Campo para calendario
      *
-     * @param string $name nombre de campo
+     * @param string $field nombre de campo
      * @param string $format formato de fecha como lo acepta jsCalendar
      * @param string $class clases adicionales
      * @param string $attrs atributos de campo
      * @param string $value valor para el campo
      * @return string
      **/
-    public static function calendar($name, $format='%d-%m-%Y', $class=null, $attrs=null, $value=null)
+    public static function calendar($field, $format='%d-%m-%Y', $class=null, $attrs=null, $value=null)
     {
         if(is_array($attrs)){
             $attrs = Tag::getAttrs($attrs);
         }
-    
-        $field = Form::getFormField($name);
-        if($field['form']) {
-            $id = "{$field['form']}_{$field['field']}";
-        } else {
-            $id = $field['field'];
-        }
-        return Form::text($name, "class=\"js-calendar $class\" $attrs", $value) . ' ' . Html::img('calendar.gif', $format, "id=\"{$id}_tigger\"");
+
+        return Form::text($field, "class=\"js-calendar $class\" $attrs", $value) . ' ' . Html::img('calendar.gif', $format, "id=\"$field.tigger\"");
     }
     
     /**
      * Lista desplegable para actualizar usando ajax
      *
-     * @param string $name nombre de campo
+     * @param string $field nombre de campo
      * @param array $data
      * @param string $update capa que se actualizara
      * @param string $action accion que se ejecutara
      * @param string $class
      * @param string | array $attrs
      **/
-    public static function updaterSelect($name, $data, $update, $action, $class=null, $attrs=null)
+    public static function updaterSelect($field, $data, $update, $action, $class=null, $attrs=null)
     {
         if(is_array($attrs)){
             $attrs = Tag::getAttrs($attrs);
         }
         
-        $field = Form::getFormField($name);
-        if($field['form']) {
-            $id = "{$field['form']}_{$field['field']}";
-        } else {
-            $id = $field['field'];
-        }
-        
-        // asigna la metadata (la accion a ejecutar y la capa que se actualizara)
-        self::$_metadata[$id] = array(
-            'action' => URL_PATH . rtrim($action, '/') . '/',
-            'update' => $update
-        );
+		// ruta a la accion
+		$action = URL_PATH . rtrim($action, '/') . '/';
 		
         // genera el campo
-        return Form::select($name, $data, "class=\"js-remote $class\" $attrs");
+        return Form::select($field, $data, "class=\"js-remote $class\" data-update=\"$update\" data-action=\"$action\" $attrs");
     }
 	
     /**
      * Lista desplegable para actualizar usando ajax que toma los valores de un array de objetos
      *
-     * @param string $name nombre de campo
+     * @param string $field nombre de campo
      * @param array $data
-     * @param string $field campo que se mostrara
+     * @param string $show campo que se mostrara
      * @param string $update capa que se actualizara
      * @param string $action accion que se ejecutara
      * @param string $blank campo en blanco
      * @param string $class
      * @param string | array $attrs
      **/
-    public static function updaterDbSelect($name, $data, $field, $update, $action, $blank=null, $class=null, $attrs=null)
+    public static function updaterDbSelect($field, $data, $show, $update, $action, $blank=null, $class=null, $attrs=null)
     {
         if(is_array($attrs)){
             $attrs = Tag::getAttrs($attrs);
         }
         
-        $field = Form::getFormField($name);
-        if($field['form']) {
-            $id = "{$field['form']}_{$field['field']}";
-        } else {
-            $id = $field['field'];
-        }
-        
-        // asigna la metadata (la accion a ejecutar y la capa que se actualizara)
-        self::$_metadata[$id] = array(
-            'action' => URL_PATH . rtrim($action, '/') . '/',
-            'update' => $update
-        );
-        
+		// ruta a la accion
+		$action = URL_PATH . rtrim($action, '/') . '/';
+		
         // genera el campo
-        return Form::dbSelect($name, $data, $field, $blank, "class=\"js-remote $class\" $attrs");
-    }
-    
-    /**
-     * Asigna una metadata
-     *
-     * @param string $name nombre de metadata
-     * @param mixed $value valor
-     **/
-    public static function setMetadata($name, $value)
-    {
-        self::$_metadata[$name] = $value;
-    }
-    
-    /**
-     * Obtiene una metadata asignada
-     *
-     * @param string $name nombre de metadata
-     * @return mixed
-     **/
-    public static function getMetadata($name)
-    {
-        if(isset(self::$_metadata[$name])) {
-            return self::$_metadata[$name];
-        } else {
-            return null;
-        }
-    }
-    
-    /**
-     * Incluye la metadata para javascript
-     *
-     * @return string
-     **/
-    public static function includeMetadata()
-    {
-        return '<script type="text/javascript"> jQuery.KumbiaPHP.metadata = ' . json_encode(self::$_metadata) . '; </script>';
+        return Form::dbSelect($field, $data, $show, $blank, "class=\"js-remote $class\" data-update=\"$update\" data-action=\"$action\" $attrs");
     }
 }
