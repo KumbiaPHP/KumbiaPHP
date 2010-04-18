4f67331543611ef3160f770ebef6215ce94e72b5
joanhey <joanhey@kumbiaphp.com>
2010-04-18 19:03:42 +0200

    Umm, parte 2 de la mejora de la clase Db. Habia quedado sin subir :p

--- core/libs/db/db.php
+++ core/libs/db/db.php
@@ -51,9 +51,8 @@ class Db
      */
     public static function factory ($database = null, $new = false)
     {
-        /**
-         * Cargo el mode para mi aplicacion
-         **/
+	
+	//Cargo el mode para mi aplicacion
         if (! $database) {
             $database = Config::get('config.application.database');
         }
@@ -81,20 +80,32 @@ class Db
 	$default = array ('port' => 0, 'dsn' => NULL, 'dbname' => NULL, 'host' => 'localhost', 'username' => NULL, 'password' => NULL);
 	$config = $config + $default;
 
-        //Cargo la clase adaptadora necesaria
+        //Si usa PDO 
         if (isset($config['pdo'])) {
-            try {
-            	$connection = new PDO($config['type'] . ":" . $config['dsn'], $config['username'], $config['password']);
+            return self::pdo($database);
+	}
+	
+	//Carga la clase adaptadora necesaria
+        $dbclass = "Db{$config['type']}";
+        if (! include_once CORE_PATH . 'libs/db/adapters/' . $config['type'] . '.php'){
+	    throw new KumbiaException("No existe la clase $dbclass, necesaria para iniciar el adaptador");
+	}
+ 
+        return new $dbclass($config);
+    }
+    
+    /**
+     * Realiza una conexión para PDO
+     *
+     * @param string $database base de datos a donde conectar
+     * @return db
+     */
+    private static function pdo($database)
+    {
+	try {
+            	return new PDO($config['type'] . ":" . $config['dsn'], $config['username'], $config['password']);
             } catch (PDOException $e) {
                 throw new KumbiaException($e->getMessage());
             }
-        } else {
-            $dbclass = "Db{$config['type']}";
-            if (! include_once CORE_PATH . 'libs/db/adapters/' . $config['type'] . '.php'){
-		throw new KumbiaException("No existe la clase $dbclass, necesaria para iniciar el adaptador");
-	    }
-        }
-
-        return new $dbclass($config);
     }
 }
