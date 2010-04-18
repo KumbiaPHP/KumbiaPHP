46bdbc1dee973b98f02aaad415e5559903cbee6f
joanhey <joanhey@kumbiaphp.com>
2010-04-18 18:57:10 +0200

    Limpieza y mejora de velocidad en la clase Db. Posiblemente podriamos crear otro singleton para los adaptadores de db.

--- core/libs/db/db.php
+++ core/libs/db/db.php
@@ -16,7 +16,7 @@
  * 
  * @category   Kumbia
  * @package    Db 
- * @copyright  Copyright (c) 2005-2009 Kumbia Team (http://www.kumbiaphp.com)
+ * @copyright  Copyright (c) 2005-2010 Kumbia Team (http://www.kumbiaphp.com)
  * @license    http://wiki.kumbiaphp.com/Licencia     New BSD License
  */
 
@@ -43,10 +43,9 @@ class Db
     protected static $_connections = array();
 
     /**
-     * Realiza una conexión directa al motor de base de datos
-     * usando el driver de Kumbia
+     * Devuelve la conexión, si no existe llama a Db::connect para crearla
      *
-     * @param boolean $new nueva conexion
+     * @param boolean $new nueva conexion //TODO mirar si es necesaria
      * @param string $database base de datos a donde conectar
      * @return db
      */
@@ -58,31 +57,30 @@ class Db
         if (! $database) {
             $database = Config::get('config.application.database');
         }
+	//Si no es una conexion nueva y existe la conexion singleton
+        if (isset(self::$_connections[$database])) {
+	    return self::$_connections[$database];
+        }
+	
+	return self::connect($database);
+    }
+
+    /**
+     * Realiza una conexión directa al motor de base de datos
+     * usando el driver de Kumbia
+     *
+     * @param string $database base de datos a donde conectar
+     * @return db
+     */
+    private static function connect($database)
+    {
         $databases = Config::read('databases');
         $config = $databases[$database];
-        /**
-         * Cargo valores por defecto para la conexion
-         * en caso de que no existan
-         **/
-        if (! isset($config['port'])) {
-            $config['port'] = 0;
-        }
-        if (! isset($config['dsn'])) {
-            $config['dsn'] = '';
-        }
-        if (! isset($config['host'])) {
-            $config['host'] = '';
-        }
-        if (! isset($config['username'])) {
-            $config['username'] = '';
-        }
-        if (! isset($config['password'])) {
-            $config['password'] = '';
-        }
-        //Si no es una conexion nueva y existe la conexion singleton
-        if (! $new && isset(self::$_connections[$database])) {
-            return self::$_connections[$database];
-        }
+
+	// carga los valores por defecto para la conexión, si no existen
+	$default = array ('port' => 0, 'dsn' => NULL, 'dbname' => NULL, 'host' => 'localhost', 'username' => NULL, 'password' => NULL);
+	$config = $config + $default;
+
         //Cargo la clase adaptadora necesaria
         if (isset($config['pdo'])) {
             try {
@@ -92,20 +90,11 @@ class Db
             }
         } else {
             $dbclass = "Db{$config['type']}";
-            //if (! class_exists($dbclass)) {
-                require_once CORE_PATH . 'libs/db/adapters/' . $config['type'] . '.php';
-            //}
-            
-            if (! class_exists($dbclass)) {
-                throw new KumbiaException("No existe la clase $dbclass, necesaria para iniciar el adaptador");
-            }
-            $connection = new $dbclass($config);
+            if (! include_once CORE_PATH . 'libs/db/adapters/' . $config['type'] . '.php'){
+		throw new KumbiaException("No existe la clase $dbclass, necesaria para iniciar el adaptador");
+	    }
         }
 
-        //Si no es para conexion nueva, la cargo en el singleton
-        if (! $new) {
-            self::$_connections[$database] = $connection;
-        }
-        return $connection;
+        return new $dbclass($config);
     }
 }
