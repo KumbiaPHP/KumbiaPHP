d5dcc91abd50665d7ddcaa6d8c7121059eca216c
Emilio Silveira <emilio.rst@gmail.com>
2010-08-30 11:13:33 -0430

    Adicionado mensaje de confirmacion por defecto a Js:link

--- core/extensions/helpers/js.php
+++ core/extensions/helpers/js.php
@@ -32,7 +32,7 @@ class Js
      * @param string | array $attrs atributos adicionales
      * @return string
      */
-    public static function link ($action, $text, $confirm, $class = NULL, $attrs = NULL)
+    public static function link ($action, $text, $confirm = '¿Está Seguro?', $class = NULL, $attrs = NULL)
     {
         if (is_array($attrs)) {
             $attrs = Tag::getAttrs($attrs);
