d70a0a7c4a469563731235f6a6a550bb773c5af1
Emilio Silveira <emilio.rst@gmail.com>
2010-09-18 23:48:42 -0430

    Se realizo un pequeño cambio mediante el cual el usuario puede modificar el comportamiento de la clase Router.
    Se realizo de manera similar a lo que se hizo anteriormente con la clase 
    View.
    
    Se coloco una clase Router en app/libs/router.php y se renombro la del 
    core a KumbiaRouter en el archivo kumbia_router.php
    
    De momento su uso principal es permitir sobre-escribir el comportamiento 
    del metodo Rewrite, de manera que su uso en herencia es bastante 
    sencillo. Considerando un ejemplo ilustrativo:
    
    En app/libs/router.php
    
    class Router extends KumbiaRouter
    {
        public static function rewrite($url)
        {
            // Aqui operaciones de reescritura adicional para la URL que se 
    deseen
    
            // En este caso obligamos a que todas las urls que vayan al 
    index
            $nuevaUrl = 'index/index';
    
            // Le pasamos la nueva url al padre para que la interprete
            return parent::rewrite($nuevaUrl);
        }
    }

--- core/kumbia/kumbia_router.php
+++ core/kumbia/kumbia_router.php
@@ -19,7 +19,7 @@
  * @copyright  Copyright (c) 2005-2009 Kumbia Team (http://www.kumbiaphp.com)
  * @license    http://wiki.kumbiaphp.com/Licencia     New BSD License
  */
-final class Router
+class KumbiaRouter
 {
 	/**
 	 * Array estatico con las variables del router
