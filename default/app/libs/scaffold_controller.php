e80ec4d64653075e0986d3bfb483f7ec4bf9d4c0
joanhey <joanhey@kumbiaphp.com>
2010-10-27 03:17:48 +0200

    Simplificado el scaffold_controller.php

--- default/app/libs/scaffold_controller.php
+++ default/app/libs/scaffold_controller.php
@@ -5,42 +5,39 @@ class ScaffoldController extends ApplicationController {
 	public $scaffold = 'kumbia';
 	public $model;
 
-    public function index($page=1)
-    {
-        $this->results = Load::model($this->model)->paginate("page: $page", 'order: id desc');
-    }
+	public function index($page=1)
+	{
+		$this->results = Load::model($this->model)->paginate("page: $page", 'order: id desc');
+	}
 
-    /**
-     * Crea un Registro
-     */
-    public function crear ()
-    {
-	$model = Util::camelcase($this->model);
-        if(Input::hasPost($this->model)){
+	/**
+	 * Crea un Registro
+	 */
+	public function crear ()
+	{
+		if(Input::hasPost($this->model)){
 
-            $obj = Load::model($this->model);
-            //En caso que falle la operación de guardar
-            if(!$obj->save(Input::post($this->model))){
-                Flash::error('Falló Operación');
-                //se hacen persistente los datos en el formulario
-                $this->result = $obj;//post($model);
-                return;
-            }
-			return Router::redirect("$this->controller_path");
-        }
+		    $obj = Load::model($this->model);
+		    //En caso que falle la operación de guardar
+		    if(!$obj->save(Input::post($this->model))){
+			Flash::error('Falló Operación');
+			//se hacen persistente los datos en el formulario
+			$this->result = $obj;
+			return;
+		    }
+		    return Router::redirect();
+		}
 		// Solo es necesario para el autoForm
 		$this->result = Load::model($this->model);
-    }
+	}
 
-    /**
-     * Edita un Registro
-     */
-    public function editar($id = null)
-    {
-    	if($id != null){
+	/**
+	 * Edita un Registro
+	 */
+	public function editar($id)
+	{
 		View::select('crear');
 
-		$model = Util::camelcase($this->model);
 		//se verifica si se ha enviado via POST los datos
 		if(Input::hasPost($this->model)){
 			$obj = Load::model($this->model);
@@ -49,36 +46,31 @@ class ScaffoldController extends ApplicationController {
 				//se hacen persistente los datos en el formulario
 				$this->result = Input::post($this->model);
 			} else {
-			   return Router::redirect("$this->controller_path");
+			   return Router::redirect();
 			}
 		}
 
 		//Aplicando la autocarga de objeto, para comenzar la edición
 		$this->result = Load::model($this->model)->find($id);
-    	} else {
 
+	}
+
+	/**
+	 * Borra un Registro
+	 */
+	public function borrar($id)
+	{
+		if (!Load::model($this->model)->delete($id)) {
+			Flash::error('Falló Operación');
 		}
-    }
+		//enrutando al index para listar los articulos
+		Router::redirect();
+	}
 
-    /**
-     * Eliminar un menu
-     *
-     * @param int $id
-     */
-    public function borrar($id = null)
-    {
-        if ($id) {
-            if (!Load::model($this->model)->delete($id)) {
-                Flash::error('Falló Operación');
-            }
-        }
-        //enrutando al index para listar los articulos
-	Router::redirect("$this->controller_path");
-	View::select(NULL, NULL);
-    }
-	public function ver($id = null) {
-       if($id){
+	/**
+	 * Ver un Registro
+	 */
+	public function ver($id) {
 		$this->result = Load::model($this->model)->find_first($id);
-       }
 	}
 }
