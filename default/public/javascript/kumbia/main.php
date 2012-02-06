fef2b13c2f966b0ee47b1a76038575c34369661c
ashrey <alberroteran@gmail.com>
2012-02-06 12:29:03 -0430

    Eliminada las caracteristicas obsoletas del código heredado (Scriptaculo y Kumbia)

--- default/public/javascript/kumbia/main.php
+++
@@ -1,6 +0,0 @@
-$Kumbia = new Object();
-$Kumbia.app = "<?= urldecode($_REQUEST['app']) ?>";
-$Kumbia.path = "<?= urldecode($_REQUEST['path']) ?>";
-$Kumbia.module = "<?= urldecode($_REQUEST['module']) ?>";
-$Kumbia.controller = "<?= $_REQUEST['controller'] ?>";
-$Kumbia.action = "<?= $_REQUEST['action'] ?>";
