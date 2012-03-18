852e5c2842d96d48644ccf52c1a18d38b48e0f90
Manuel Aguirre <programador.manuel@gmail.com>
2012-03-18 17:26:54 -0430

    mejorando el phpdoc y limpiando estilo de codificación

--- core/libs/db/adapters/pdo/interface.php
+++ core/libs/db/adapters/pdo/interface.php
@@ -12,51 +12,84 @@
  * obtain it through the world-wide-web, please send an email
  * to license@kumbiaphp.com so we can send you a copy immediately.
  *
- * Esta interface expone los metodos que se deben implementar en un driver
- * de Kumbia
- * 
  * @category   Kumbia
  * @package    Db
  * @subpackage Adapters 
  * @copyright  Copyright (c) 2005-2012 Kumbia Team (http://www.kumbiaphp.com)
  * @license    http://wiki.kumbiaphp.com/Licencia     New BSD License
  */
-interface DbPdoInterface {
-	public function initialize();
-	public function connect($config);
-	public function query($sql);
-	public function exec($sql);
-	public function fetch_array($resultQuery='', $opt='');
-	public function close();
-	/**
-	 * Este metodo no esta soportado por PDO, usar fetch_all y luego contar con count()
-	 *
-	 * @param resource $result_query
-	 */
-	public function num_rows($resultQuery='');
-	public function field_name($number, $resultQuery='');
-	/**
-	 * Este metodo no esta soportado por PDO, usar fetch_all y luego contar con count()
-	 *
-	 * @param resource $result_query
-	 */
-	public function data_seek($number, $resultQuery='');
-	public function affected_rows($result_query='');
-	public function error($err='');
-	public function no_error($number=0);
-	public function in_query($sql);
-	public function in_query_assoc($sql);
-	public function in_query_num($sql);
-	public function fetch_one($sql);
-	public function fetch_all($sql);
-	public function last_insert_id($name='');
-	public function insert($table, $values, $pk='');
-	public function update($table, $fields, $values, $where_condition=null);
-	public function delete($table, $where_condition);
-	public function limit($sql);
-	public function create_table($table, $definition, $index=array());
-	public function drop_table($table, $if_exists=false);
-	public function table_exists($table, $schema='');
-	public function describe_table($table, $schema='');
 
+/**
+ * Interfaz para los adaptadores de bases de datos PDO
+ *
+ * Esta interface expone los metodos que se deben implementar en un driver
+ * de Kumbia
+ *
+ * @category   Kumbia
+ * @package    Db
+ * @subpackage Adapters
+ */
+interface DbPdoInterface
+{
+    public function initialize();
+
+    public function connect($config);
+
+    public function query($sql);
+
+    public function exec($sql);
+
+    public function fetch_array($resultQuery='', $opt='');
+
+    public function close();
+
+    /**
+     * Este metodo no esta soportado por PDO, usar fetch_all y luego contar con count()
+     *
+     * @param resource $result_query
+     */
+    public function num_rows($resultQuery='');
+
+    public function field_name($number, $resultQuery='');
+
+    /**
+     * Este metodo no esta soportado por PDO, usar fetch_all y luego contar con count()
+     *
+     * @param resource $result_query
+     */
+    public function data_seek($number, $resultQuery='');
+
+    public function affected_rows($result_query='');
+
+    public function error($err='');
+
+    public function no_error($number=0);
+
+    public function in_query($sql);
+
+    public function in_query_assoc($sql);
+
+    public function in_query_num($sql);
+
+    public function fetch_one($sql);
+
+    public function fetch_all($sql);
+
+    public function last_insert_id($name='');
+
+    public function insert($table, $values, $pk='');
+
+    public function update($table, $fields, $values, $where_condition=null);
+
+    public function delete($table, $where_condition);
+
+    public function limit($sql);
+
+    public function create_table($table, $definition, $index=array());
+
+    public function drop_table($table, $if_exists=false);
+
+    public function table_exists($table, $schema='');
+
+    public function describe_table($table, $schema='');
 }
--- core/libs/db/db_base_interface.php
+++ core/libs/db/db_base_interface.php
@@ -12,42 +12,76 @@
  * obtain it through the world-wide-web, please send an email
  * to license@kumbiaphp.com so we can send you a copy immediately.
  *
- * Esta interface expone los metodos que se deben implementar en un driver
- * de KumbiaPHP
- * 
  * @category   Kumbia
  * @package    Db 
  * @copyright  Copyright (c) 2005-2012 Kumbia Team (http://www.kumbiaphp.com)
  * @license    http://wiki.kumbiaphp.com/Licencia     New BSD License
  */
+
+/**
+ * Interfaz para los adaptadores de bases de datos
+ *
+ * Esta interface expone los metodos que se deben implementar en un driver
+ * de KumbiaPHP
+ *
+ * @category   Kumbia
+ * @package    Db 
+ */
 interface DbBaseInterface
 {
-    public function connect ($config);
-    public function query ($sql);
-    public function fetch_array ($resultQuery = '', $opt = '');
-    public function close ();
-    public function num_rows ($resultQuery = '');
-    public function field_name ($number, $resultQuery = '');
-    public function data_seek ($number, $resultQuery = '');
-    public function affected_rows ($result_query = '');
-    public function error ($err = '');
-    public function no_error ();
-    public function in_query ($sql);
-    public function in_query_assoc ($sql);
-    public function in_query_num ($sql);
-    public function fetch_one ($sql);
-    public function fetch_all ($sql);
-    public function insert ($table, $values, $pk = '');
-    public function update ($table, $fields, $values, $where_condition = null);
-    public function delete ($table, $where_condition);
-    public function limit ($sql);
-    public function begin ();
-    public function rollback ();
-    public function commit ();
-    public function list_tables ();
-    public function describe_table ($table, $schema = '');
-    public function last_insert_id ($table = '', $primary_key = '');
-    public function create_table ($table, $definition, $index = array());
-    public function drop_table ($table, $if_exists = false);
-    public function table_exists ($table, $schema = '');
+    public function connect($config);
+
+    public function query($sql);
+
+    public function fetch_array($resultQuery = '', $opt = '');
+
+    public function close();
+
+    public function num_rows($resultQuery = '');
+
+    public function field_name($number, $resultQuery = '');
+
+    public function data_seek($number, $resultQuery = '');
+
+    public function affected_rows($result_query = '');
+
+    public function error($err = '');
+
+    public function no_error();
+
+    public function in_query($sql);
+
+    public function in_query_assoc($sql);
+
+    public function in_query_num($sql);
+
+    public function fetch_one($sql);
+
+    public function fetch_all($sql);
+
+    public function insert($table, $values, $pk = '');
+
+    public function update($table, $fields, $values, $where_condition = null);
+
+    public function delete($table, $where_condition);
+
+    public function limit($sql);
+
+    public function begin();
+
+    public function rollback();
+
+    public function commit();
+
+    public function list_tables();
+
+    public function describe_table($table, $schema = '');
+
+    public function last_insert_id($table = '', $primary_key = '');
+
+    public function create_table($table, $definition, $index = array());
+
+    public function drop_table($table, $if_exists = false);
+
+    public function table_exists($table, $schema = '');
 }
