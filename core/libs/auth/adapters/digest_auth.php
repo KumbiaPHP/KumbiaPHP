181a133350b7391471444ff529d74dc3fa99f107
Manuel Aguirre <programador.manuel@gmail.com>
2012-03-18 14:28:02 -0430

    mejorando el phpdoc y limpiando estilo de codificación en auth y acl

--- core/libs/auth/adapters/digest_auth.php
+++ core/libs/auth/adapters/digest_auth.php
@@ -25,126 +25,123 @@
  * @category Kumbia
  * @package Auth
  * @subpackage Adapters
- * @copyright  Copyright (c) 2005-2012 Kumbia Team (http://www.kumbiaphp.com)
- * @license    http://wiki.kumbiaphp.com/Licencia     New BSD License
  * @link http://en.wikipedia.org/wiki/Digest_access_authentication
  */
-class DigestAuth implements AuthInterface {
+class DigestAuth implements AuthInterface
+{
 
-	/**
-	 * Nombre del archivo (si es utilizado)
-	 *
-	 * @var string
-	 */
-	private $filename;
+    /**
+     * Nombre del archivo (si es utilizado)
+     *
+     * @var string
+     */
+    private $filename;
+    /**
+     * Servidor de autenticaciï¿½n (si es utilizado)
+     *
+     * @var string
+     */
+    private $server;
+    /**
+     * Nombre de usuario para conectar al servidor de autenticacion (si es utilizado)
+     *
+     * @var string
+     */
+    private $username;
+    /**
+     * Password de usuario para conectar al servidor de autenticacion (si es utilizado)
+     *
+     * @var string
+     */
+    private $password;
+    /**
+     * Realm encontrado
+     *
+     * @var string
+     */
+    private $realm;
 
-	/**
-	 * Servidor de autenticación (si es utilizado)
-	 *
-	 * @var string
-	 */
-	private $server;
+    /**
+     * Constructor del adaptador
+     *
+     * @param $auth
+     * @param $extra_args
+     */
+    public function __construct($auth, $extra_args)
+    {
+        foreach (array('filename') as $param) {
+            if (isset($extra_args[$param])) {
+                $this->$param = $extra_args[$param];
+            } else {
+                throw new KumbiaException("Debe especificar el parï¿½metro '$param' en los parï¿½metros");
+            }
+        }
+        foreach (array('username', 'password') as $param) {
+            if (isset($extra_args[$param])) {
+                $this->$param = $extra_args[$param];
+            }
+        }
+    }
 
-	/**
-	 * Nombre de usuario para conectar al servidor de autenticacion (si es utilizado)
-	 *
-	 * @var string
-	 */
-	private $username;
+    /**
+     * Obtiene los datos de identidad obtenidos al autenticar
+     *
+     */
+    public function get_identity()
+    {
+        $identity = array("username" => $this->username, "realm" => $this->realm);
+        return $identity;
+    }
 
-	/**
-	 * Password de usuario para conectar al servidor de autenticacion (si es utilizado)
-	 *
-	 * @var string
-	 */
-	private $password;
+    /**
+     * Autentica un usuario usando el adaptador
+     *
+     * @return boolean
+     */
+    public function authenticate()
+    {
+        $this->resource = @fopen($this->filename, "r");
+        if ($this->resource === false) {
+            throw new KumbiaException("No existe o no se puede cargar el archivo '{$this->filename}'");
+        }
 
-	/**
-	 * Realm encontrado
-	 *
-	 * @var string
-	 */
-	private $realm;
+        $exists_user = false;
+        while (!feof($this->resource)) {
+            $line = fgets($this->resource);
+            $data = explode(":", $line);
 
+            if ($data[0] == $this->username) {
+                if (trim($data[2]) == md5($this->password)) {
+                    $this->realm = $data[1];
+                    $exists_user = true;
+                    break;
+                }
+            }
+        }
+        return $exists_user;
+    }
 
-	/**
-	 * Constructor del adaptador
-	 *
-	 * @param $auth
-	 * @param $extra_args
-	 */
-	public function __construct($auth, $extra_args){
-		foreach(array('filename') as $param){
-			if(isset($extra_args[$param])){
-				$this->$param = $extra_args[$param];
-			} else {
-				throw new KumbiaException("Debe especificar el parámetro '$param' en los parámetros");
-			}
-		}
-		foreach(array('username', 'password') as $param){
-			if(isset($extra_args[$param])){
-				$this->$param = $extra_args[$param];
-			}
-		}
-	}
+    /**
+     * Asigna los valores de los parametros al objeto autenticador
+     *
+     * @param array $extra_args
+     */
+    public function set_params($extra_args)
+    {
+        foreach (array('filename', 'username', 'password') as $param) {
+            if (isset($extra_args[$param])) {
+                $this->$param = $extra_args[$param];
+            }
+        }
+    }
 
-	/**
-	 * Obtiene los datos de identidad obtenidos al autenticar
-	 *
-	 */
-	public function get_identity(){
-		$identity = array("username" => $this->username, "realm" => $this->realm);
-		return $identity;
-	}
-
-	/**
-	 * Autentica un usuario usando el adaptador
-	 *
-	 * @return boolean
-	 */
-	public function authenticate(){
-		$this->resource = @fopen($this->filename, "r");
-		if($this->resource===false){
-			throw new KumbiaException("No existe o no se puede cargar el archivo '{$this->filename}'");
-		}
-
-		$exists_user = false;
-		while(!feof($this->resource)){
-			$line = fgets($this->resource);
-			$data = explode(":", $line);
-
-			if($data[0]==$this->username){
-				if(trim($data[2])==md5($this->password)){
-					$this->realm = $data[1];
-					$exists_user = true;
-					break;
-				}
-			}
-		}
-		return $exists_user;
-	}
-
-
-	/**
-	 * Asigna los valores de los parametros al objeto autenticador
-	 *
-	 * @param array $extra_args
-	 */
-	public function set_params($extra_args){
-		foreach(array('filename', 'username', 'password') as $param){
-			if(isset($extra_args[$param])){
-				$this->$param = $extra_args[$param];
-			}
-		}
-	}
-
-
-	/**
-	 * Limpia el objeto cerrando la conexion si esta existe
-	 *
-	 */
-	public function __destruct(){
-		@fclose($this->resource);
-	}
+    /**
+     * Limpia el objeto cerrando la conexion si esta existe
+     *
+     */
+    public function __destruct()
+    {
+        @fclose($this->resource);
+    }
 
 }
