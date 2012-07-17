c10e5fa90a41159b1aad5e0a596454716da707f8
Manuel Aguirre <programador.manuel@gmail.com>
2012-07-16 22:14:42 -0430

    corregido bug al obtener el nombre de la tabla intermendia en una asociacion
    cuando se trabaja con asociaciones y modelos dentro de carpetas
    la variable through tenia el nombre de la carpeta, ahora se ha quitado.

--- core/libs/kumbia_active_record/kumbia_active_record.php
+++ core/libs/kumbia_active_record/kumbia_active_record.php
@@ -391,6 +391,9 @@ class KumbiaActiveRecord
                 } else {
                     $relation->through = "{$relation_source}_{$this->source}";
                 }
+            }else{
+                $through = explode('/', $relation->through);
+                $relation->through = end($through);
             }
             if ($this->{$this->primary_key[0]}) {
                 return $relation_model->find_all_by_sql("SELECT $relation_source.* FROM $relation_source, {$relation->through}, $source
