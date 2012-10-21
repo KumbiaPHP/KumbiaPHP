f26569cd78d4884829e9b9eccd3bb17dd6b31b65
ashrey <aberroteran@live.com>
2012-10-21 18:46:59 -0430

    Añadidas funciones para obtener la IP y el UserAgent del usuario

--- core/libs/input/input.php
+++ core/libs/input/input.php
@@ -12,143 +12,161 @@
  * obtain it through the world-wide-web, please send an email
  * to license@kumbiaphp.com so we can send you a copy immediately.
  *
+ * Clase para manejar los datos del request
+ * 
  * @category   Kumbia
  * @package    Input
  * @copyright  Copyright (c) 2005-2012 Kumbia Team (http://www.kumbiaphp.com)
  * @license    http://wiki.kumbiaphp.com/Licencia     New BSD License
  */
-
-/**
- * Clase para manejar los datos del request
- *
- * @category   Kumbia
- * @package    Input
- */
-class Input
+class Input 
 {
+	/**
+	 * Verifica o obtiene el metodo de la peticion
+	 *
+	 * @param string $method
+	 * @return mixed
+	 */
+	public static function is($method = NULL)
+	{
+		if($method){			
+			return $method == $_SERVER['REQUEST_METHOD'];
+		}
+		return $_SERVER['REQUEST_METHOD'];
+	}
+        
+	/**
+	 * Indica si el request es AJAX
+	 *
+	 * @return Bolean
+	 */
+	public static function isAjax()
+	{
+		return (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest');
+	}
+        
+	/**
+	 * Obtiene un valor del arreglo $_POST
+	 *
+	 * @param string $var
+	 * @return mixed
+	 */
+	public static function post($var)
+	{
+		return filter_has_var(INPUT_POST, $var) ? $_POST[$var] : NULL;
+	}
+
+	/**
+	 * Obtiene un valor del arreglo $_GET, aplica el filtro FILTER_SANITIZE_STRING
+	 * por defecto
+	 *
+	 * @param string $var
+	 * @return mixed
+	 */
+	public static function get($var = NULL)
+	{
+		if($var){
+			$value = filter_has_var(INPUT_GET, $var) ? filter_input(INPUT_GET, $var, FILTER_SANITIZE_STRING) : NULL;
+		} else {
+			$value = filter_input_array (INPUT_GET, FILTER_SANITIZE_STRING);
+		}
+			
+		return $value;
+	}
+
+	/**
+	 * Obtiene un valor del arreglo $_REQUEST
+ 	 *
+	 * @param string $var
+	 * @return mixed
+	 */
+	public static function request($var)
+	{
+		return isset($_REQUEST[$var]) ? $_REQUEST[$var] : NULL;
+	}
+
+	/**
+	 * Verifica si existe el elemento indicado en $_POST
+	 *
+	 * @param string $var elemento a verificar
+	 * @return boolean
+	 */
+	public static function hasPost($var) 
+	{
+		return filter_has_var(INPUT_POST, $var);
+	}
+
+	/**
+	 * Verifica si existe el elemento indicado en $_GET
+	 *
+	 * @param string $var elemento a verificar
+	 * @return boolean
+	 */
+	public static function hasGet($var)
+	{
+		return filter_has_var(INPUT_GET, $var);
+	}
+
+	/**
+	 * Verifica si existe el elemento indicado en $_REQUEST
+	 *
+	 * @param string $var elemento a verificar
+	 * @return boolean
+	 */
+	public static function hasRequest($var) 
+	{
+		return isset($_REQUEST[$var]);
+	}
+        
+	/**
+	 * Elimina elemento indicado en $_POST
+	 *
+	 * @param string $var elemento a verificar
+	 * @return boolean
+	 */
+	public static function delete($var = NULL) 
+	{
+		if($var){
+			unset($_POST[$var]);
+		} else {
+			unset($_POST);
+		}
+	}
+	
+    /**
+    * Permite Obtener el Agente de Usuario (User Agent)
+    * @return String
+    */
+    public static userAgent(){
+        return $_SERVER['HTTP_USER_AGENT'];
+    }
 
     /**
-     * Verifica o obtiene el metodo de la peticion
-     *
-     * @param string $method
-     * @return mixed
-     */
-    public static function is($method = NULL)
-    {
-        if ($method) {
-            return $method == $_SERVER['REQUEST_METHOD'];
+    * Permite obtene la IP del cliente, aún cuando usa proxy
+    * @return String
+    */
+    public static ip(){
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])){
+            return $_SERVER['HTTP_CLIENT_IP'];
+        }elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+            return $_SERVER['HTTP_X_FORWARDED_FOR'];
+        }else{
+            return $_SERVER['REMOTE_ADDR'];    
         }
-        return $_SERVER['REQUEST_METHOD'];
     }
+
 
     /**
-     * Indica si el request es AJAX
-     *
-     * @return Bolean
-     */
-    public static function isAjax()
+	 * Obtiene y filtra un valor del arreglo $_REQUEST
+	 * Por defecto, usa SANITIZE
+ 	 *
+	 * @param string $var
+	 * @return mixed
+	 */
+	public static function filter($var)
     {
-        return (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest');
+		//TODO
     }
 
-    /**
-     * Obtiene un valor del arreglo $_POST
-     *
-     * @param string $var
-     * @return mixed
-     */
-    public static function post($var)
-    {
-        return filter_has_var(INPUT_POST, $var) ? $_POST[$var] : NULL;
-    }
-
-    /**
-     * Obtiene un valor del arreglo $_GET, aplica el filtro FILTER_SANITIZE_STRING
-     * por defecto
-     *
-     * @param string $var
-     * @return mixed
-     */
-    public static function get($var = NULL)
-    {
-        if ($var) {
-            $value = filter_has_var(INPUT_GET, $var) ? filter_input(INPUT_GET, $var, FILTER_SANITIZE_STRING) : NULL;
-        } else {
-            $value = filter_input_array(INPUT_GET, FILTER_SANITIZE_STRING);
-        }
-
-        return $value;
-    }
-
-    /**
-     * Obtiene un valor del arreglo $_REQUEST
-     *
-     * @param string $var
-     * @return mixed
-     */
-    public static function request($var)
-    {
-        return isset($_REQUEST[$var]) ? $_REQUEST[$var] : NULL;
-    }
-
-    /**
-     * Verifica si existe el elemento indicado en $_POST
-     *
-     * @param string $var elemento a verificar
-     * @return boolean
-     */
-    public static function hasPost($var)
-    {
-        return filter_has_var(INPUT_POST, $var);
-    }
-
-    /**
-     * Verifica si existe el elemento indicado en $_GET
-     *
-     * @param string $var elemento a verificar
-     * @return boolean
-     */
-    public static function hasGet($var)
-    {
-        return filter_has_var(INPUT_GET, $var);
-    }
-
-    /**
-     * Verifica si existe el elemento indicado en $_REQUEST
-     *
-     * @param string $var elemento a verificar
-     * @return boolean
-     */
-    public static function hasRequest($var)
-    {
-        return isset($_REQUEST[$var]);
-    }
-
-    /**
-     * Elimina elemento indicado en $_POST
-     *
-     * @param string $var elemento a verificar
-     * @return boolean
-     */
-    public static function delete($var = NULL)
-    {
-        if ($var) {
-            unset($_POST[$var]);
-        } else {
-            unset($_POST);
-        }
-    }
-
-    /**
-     * Obtiene y filtra un valor del arreglo $_REQUEST
-     * Por defecto, usa SANITIZE
-     *
-     * @param string $var
-     * @return mixed
-     */
-    public static function filter($var)
-    {
-        //TODO
-    }
-
+    
 }
