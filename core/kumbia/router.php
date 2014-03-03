7c60031251a3497300023d3ae0f8e91aa6603dd9
joanhey <joanhey@kumbiaphp.com>
2014-03-03 19:45:29 +0100

    Mejora del phpdoc

--- core/kumbia/router.php
+++ core/kumbia/router.php
@@ -231,13 +231,10 @@ final class Router
      * ej.
      * <code>Router::get()</code>
      *
-     * @param ninguno
-     * @return array con todas los atributos y sus valores
-     *
      * ej.
      * <code>Router::get('controller')</code>
      *
-     * @param string  un atributo: route, module, controller, action, parameters o routed
+     * @param string  (opcional) un atributo: route, module, controller, action, parameters o routed
      * @return array|string con el valor del atributo
      */
     public static function get($var = null)
