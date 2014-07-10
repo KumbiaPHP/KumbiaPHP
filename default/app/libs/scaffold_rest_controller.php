c07f31a9fca0f976d5d1ab9a45b8ba87e74bed0c
Henry Stivens Adarme <henrystivens@kts-compumax01.(none)>
2014-07-10 16:33:51 -0500

    Controlador base para la construcción de API REST para modelos rapidamente

---
+++ default/app/libs/scaffold_rest_controller.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * Controlador base para la construcción de API REST para modelos rapidamente
+ *
+ * @category Kumbia
+ * @package Controller
+ */
+class ScaffoldRestController extends RestController
+{
+    
+    public $model;
+    
+    /**
+     * Retorna un registro a través de su $id 
+     * metodo get objeto/:id
+     */
+    public function get($id){        
+        $this->data = Load::model($this->model)->find((int)$id);
+    }
+ 
+    /**
+     * Lista los registros
+     * metodo get libros/
+     */
+    public function getAll(){	
+        $this->data = Load::model($this->model)->find();
+    }
+ 
+    /**
+     * Crea un nuevo registro
+     * metodo post libros/
+     */
+    public function post(){
+	$obj = Load::model($this->model);
+	if($obj->save($this->param())){
+		$this->setCode(201);
+		$this->data = $obj;
+	}else{
+		$this->data = $this->error("error inesperado", 400);
+	}
+    }
+ 
+    /**
+     * Modifica un registro por $id
+     * metodo put libros/:id
+     */
+    public function put($id){
+	$obj = Load::model($this->model);
+	$obj = $obj->find((int)$id);
+	if($obj->save($this->param())){
+		$this->setCode(202);
+		$this->data = $obj;
+	}else{
+		$this->data = $this->error("error inesperado", 400);
+	}
+    }
+ 
+    /**
+     * Elimina un registro por $id
+     * metodo delete libros/:id
+     */
+    public function delete($id){
+	$obj = Load::model($this->model);
+	if($obj->delete((int)$id)){
+		$this->setCode(200);
+		$this->data = array();
+	}else{
+		$this->data = $this->error("error inesperado", 400);
+	}	
+    }
+}
