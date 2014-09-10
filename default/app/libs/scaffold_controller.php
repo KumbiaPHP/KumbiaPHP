0ff534e52125cbf50fbabc5bb75d763d7fc99dec
Govani <govaniso@hotmail.com>
2014-09-09 21:38:53 -0500

    add documentation
    Agregandando documentacion.

--- default/app/libs/scaffold_controller.php
+++ default/app/libs/scaffold_controller.php
@@ -1,4 +1,24 @@
 <?php
+/**
+ * KumbiaPHP web & app Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://wiki.kumbiaphp.com/Licencia
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@kumbiaphp.com so we can send you a copy immediately.
+ *
+ * Cargador Selectiva
+ *
+ * @category   Kumbia
+ * @package    Kumbia
+ * @copyright  Copyright (c) 2005-2014 Kumbia Team (http://www.kumbiaphp.com)
+ * @license    http://wiki.kumbiaphp.com/Licencia     New BSD License
+ */
 
 /**
  * Controlador base para la construcción de CRUD para modelos rapidamente
@@ -14,7 +34,7 @@ class ScaffoldController extends AppController
 
     public function index($page=1)
     {
-        $this->results = Load::model($this->model)->paginate("page: $page","per_page: 10, 'order: id desc');
+        $this->results = Load::model($this->model)->paginate("page: $page","per_page: 10", 'order: id desc');
     }
 
     /**
