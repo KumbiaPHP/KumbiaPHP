2a22d354701d9b683e7ed23556165c8897f1e433
joanhey <joanhey@kumbiaphp.com>
2015-06-16 21:16:36 +0200

    Removed eh

--- core/kumbia/kumbia_view.php
+++ core/kumbia/kumbia_view.php
@@ -369,16 +369,3 @@ function h($s, $charset = APP_CHARSET)
 {
     return htmlspecialchars($s, ENT_QUOTES, $charset);
 }
-
-/**
- * Atajo para echo + htmlspecialchars, por defecto toma el charset de la
- * aplicacion
- *
- * @param string $s
- * @param string $charset
- * @return string
- */
-function eh($s, $charset = APP_CHARSET)
-{
-    echo htmlspecialchars($s, ENT_QUOTES, $charset);
-}
