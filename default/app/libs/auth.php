57985e60e94dabc1b0510d0d69e054ef0cb908a3
Alberto Berroteran <aberroteran@live.com>
2015-06-30 20:07:31 -0430

    Add kumbia Auth base

---
+++ default/app/libs/auth.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Auth
+ *
+ * @category Kumbia
+ * @package Auth
+ * @author kumbiaPHP Team
+ */
+class Auth implements \KumbiaAuthInterface {
+	public function login(Array $arg = array()) {
+		/**
+	 * You need implement this method
+	 */
+	}
+}
