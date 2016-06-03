80a6982f0c5aaee9bca2ba8e24ea8b49caba0380
manuelj555 <programador.manuel@gmail.com>
2016-06-03 18:16:46 -0430

    Configurando phpunit para crear tests de las clases del Core.
    Se crea la primera clase de tests SessionTest.php

---
+++ core/tests/bootstrap.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * KumbiaPHP web & app Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://wiki.kumbiaphp.com/Licencia
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@kumbiaphp.com so we can send you a copy immediately.
+ *
+ * @category   Kumbia
+ * @package    Session
+ * @copyright  Copyright (c) 2005 - 2016 Kumbia Team (http://www.kumbiaphp.com)
+ * @license    http://wiki.kumbiaphp.com/Licencia     New BSD License
+ */
+
+if (!isset($_SESSION)) { session_start(); }
+
+defined('CORE_PATH') or define('CORE_PATH', dirname(__DIR__) . '/');
+defined('APP_PATH') or define('APP_PATH', __DIR__ . '/');
+
+require_once CORE_PATH.'kumbia/autoload.php';
+require_once __DIR__.'/../../vendor/autoload.php';
