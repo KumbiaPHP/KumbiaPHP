99ae91a6278ba855bb73b7343d71ab57d2bd175d
joanhey <joanhey@kumbiaphp.com>
2017-02-10 08:10:30 +0100

    Session now uses constant

--- core/libs/session/session.php
+++ core/libs/session/session.php
@@ -1,6 +1,7 @@
 <?php
+
 /**
- * KumbiaPHP web & app Framework
+* KumbiaPHP web & app Framework
  *
  * LICENSE
  *
@@ -18,66 +19,74 @@
  * @license    http://wiki.kumbiaphp.com/Licencia     New BSD License
  */
 
+
 /**
- * Modelo orientado a objetos para el acceso a datos en Sesiones
+* Modelo orientado a objetos para el acceso a datos en Sesiones
  *
  * @category   Kumbia
  * @package    Session
  */
 
+
 /*Session start*/
-if (session_status() !== PHP_SESSION_ACTIVE) {session_start();}
+if (session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
 
 class Session
 {
-
-    /**
-     * Crear o especificar el valor para un indice de la sesión
-     * actual
-     *
-     * @param string $index
-     * @param string $namespace
-     */
-    public static function set($index, $value, $namespace='default')
-    {
-        $_SESSION['KUMBIA_SESSION'][APP_PATH][$namespace][$index] = $value;
-    }
-
-    /**
-     * Obtener el valor para un indice de la sesión
-     *
-     * @param string $index
-     * @param string $namespace
-     * @return mixed
-     */
-    public static function get($index, $namespace='default')
-    {
-        if (isset($_SESSION['KUMBIA_SESSION'][APP_PATH][$namespace][$index])) {
-            return $_SESSION['KUMBIA_SESSION'][APP_PATH][$namespace][$index];
-        }
-    }
-
-    /**
-     * Elimina un indice
-     *
-     * @param string $index
-     * @param string $namespace
-     */
-    public static function delete($index, $namespace='default')
-    {
-        unset($_SESSION['KUMBIA_SESSION'][APP_PATH][$namespace][$index]);
-    }
-
-    /**
-     * Verifica si el indice esta cargado en sesión
-     *
-     * @param string $index
-     * @param string $namespace
-     * @return boolean
-     */
-    public static function has($index, $namespace='default')
-    {
-        return isset($_SESSION['KUMBIA_SESSION'][APP_PATH][$namespace][$index]);
-    }
-
+	const SESSION = 'KUMBIA_SESSION';
+	
+	/**
+	* Crear o especificar el valor para un indice de la sesión
+	     * actual
+	     *
+	     * @param string $index
+	     * @param string $namespace
+	     */
+	    public static function set($index, $value, $namespace = 'default')
+	    {
+		$_SESSION[SESSION][APP_PATH][$namespace][$index] = $value;
+	}
+	
+	
+	/**
+	* Obtener el valor para un indice de la sesión
+	     *
+	     * @param string $index
+	     * @param string $namespace
+	     * @return mixed
+	     */
+	    public static function get($index, $namespace='default')
+	    {
+		if (isset($_SESSION[SESSION][APP_PATH][$namespace][$index])) {
+			return $_SESSION[SESSION][APP_PATH][$namespace][$index];
+		}
+	}
+	
+	
+	/**
+	* Elimina un indice
+	     *
+	     * @param string $index
+	     * @param string $namespace
+	     */
+	    public static function delete($index, $namespace='default')
+	    {
+		unset($_SESSION[SESSION][APP_PATH][$namespace][$index]);
+	}
+	
+	
+	/**
+	* Verifica si el indice esta cargado en sesión
+	     *
+	     * @param string $index
+	     * @param string $namespace
+	     * @return boolean
+	     */
+	    public static function has($index, $namespace='default')
+	    {
+		return isset($_SESSION[SESSION][APP_PATH][$namespace][$index]);
+	}
+	
 }
