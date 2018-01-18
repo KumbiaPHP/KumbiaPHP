c214263e962a3a5aeabd9a6a731b014e754d5484
vurro <compacto_full@hotmail.com>
2018-01-18 01:04:51 -0300

    FOUND 1 ERROR AND 1 WARNING AFFECTING 2 LINES

--- core/console/kumbia.php
+++ core/console/kumbia.php
@@ -19,13 +19,9 @@
  * @copyright  Copyright (c) 2005 - 2017 Kumbia Team (http://www.kumbiaphp.com)
  * @license    http://wiki.kumbiaphp.com/Licencia     New BSD License
  */
-// Define el CORE_PATH
-define('CORE_PATH', dirname(__DIR__) . '/');
 
-/**
- * @see Console
- */
-require CORE_PATH . 'kumbia/console.php';
+define('CORE_PATH', dirname(__DIR__).'/');
 
-// Ejecuta el despachador
+require_once(CORE_PATH.'kumbia/console.php');
+
 Console::dispatch($argv);
