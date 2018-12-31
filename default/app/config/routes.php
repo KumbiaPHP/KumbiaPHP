73e0cd41642d2492648294129df863312a905e71
Henry Stivens Adarme M <henry.stivens@gmail.com>
2018-12-30 21:17:13 -0500

    The blocks of the comments were changed in routes.php

--- default/app/config/routes.php
+++ default/app/config/routes.php
@@ -1,44 +1,32 @@
 <?php
-
-/*
-|-------------------------------------------------------------------------------
-| KumbiaPHP Static Routing Configuration
-| Usa este archivo para definir el enrutamiento estatico entre
-| controladores y sus acciones
-|-------------------------------------------------------------------------------
-| Un controlador se puede enrutar a otro controlador utilizando '*' como
-| comodin así:
-| '/controlador1/accion1/valor_id1'  =>  'controlador2/accion2/valor_id2'
-|-------------------------------------------------------------------------------
-*/
-
+/**
+ * KumbiaPHP Web Framework
+ * Archivo de rutas (Opcional)
+ * 
+ * Usa este archivo para definir el enrutamiento estatico entre
+ * controladores y sus acciones.Un controlador se puede enrutar a 
+ * otro controlador utilizando '*' como comodin así:
+ * 
+ * '/controlador1/accion1/valor_id1'  =>  'controlador2/accion2/valor_id2'
+ * 
+ * Ej:
+ * Enrutar cualquier petición a posts/adicionar a posts/insertar/*
+ * '/posts/adicionar/*' => 'posts/insertar/*'
+ * 
+ * Otros ejemplos:
+ * 
+ * '/prueba/ruta1/*' => 'prueba/ruta2/*',
+ * '/prueba/ruta2/*' => 'prueba/ruta3/*',
+ */
 $routes['routes'] = [
-	/*
-	| Ej:
-	| Enrutar cualquier petición a posts/adicionar a posts/insertar/*
-	*/
-	'/posts/adicionar/*' =>	'posts/insertar/*',
-
-	/*
-	| Otros ejemplos
-	*/
-	'/prueba/ruta1/*' => 'prueba/ruta2/*',
-	'/prueba/ruta2/*' => 'prueba/ruta3/*',
-
-	/*
-	|-------------------------------------------------------------------------------
-	| Muestra la info relacionado con el framework
-	|-------------------------------------------------------------------------------
-	*/
-	'/' => 'index/index',
-
-	/*
-	|-------------------------------------------------------------------------------
-	| Status del config.php | config.ini
-	|-------------------------------------------------------------------------------
-	*/
-	'/status' => 'pages/show/kumbia/status'
-
+    /**
+     * Muestra la info relacionado con el framework
+     */
+    '/' => 'index/index',
+    /**
+     * Status del config.php/config.ini
+     */
+    '/status' => 'pages/show/kumbia/status'
 ];
 
 return $routes;
