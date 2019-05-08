e1798728f381de888dcefbb0e635939cd8e3778c
joanhey <joanhey@kumbiaphp.com>
2019-05-09 00:32:55 +0200

    Typos

--- default/app/libs/app_controller.php
+++ default/app/libs/app_controller.php
@@ -7,8 +7,8 @@ require_once CORE_PATH . 'kumbia/controller.php';
 /**
  * Controlador principal que heredan los controladores
  *
- * Todas las controladores heredan de esta clase en un nivel superior
- * por lo tanto los metodos aqui definidos estan disponibles para
+ * Todos las controladores heredan de esta clase en un nivel superior
+ * por lo tanto los métodos aquií definidos estan disponibles para
  * cualquier controlador.
  *
  * @category Kumbia
