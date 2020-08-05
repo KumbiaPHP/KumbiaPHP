6dcdaf1fe07616090828c849d6d6b977b7d129e3
Joanhey <joanhey@kumbiaphp.com>
2020-08-05 20:20:35 +0200

    Fix variable name

--- default/public/javascript/jquery/jquery.kumbiaphp.js
+++ default/public/javascript/jquery/jquery.kumbiaphp.js
@@ -263,7 +263,7 @@
 		initialize: function () {
 			// Obtiene el publicPath
 			let src = $('script:last').attr('src');
-			this.publicPath = src.slice(0, url.lastIndexOf('javascript/jquery/'));
+			this.publicPath = src.slice(0, src.lastIndexOf('javascript/jquery/'));
 
 			// Enlaza a los métodos por defecto
 			$(function () {
