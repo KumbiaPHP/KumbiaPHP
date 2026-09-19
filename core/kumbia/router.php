13dca332594277a12d00e8c0032a9ff5802e874a
Henry Stivens Adarme Muñoz <henry.stivens@gmail.com>
2026-09-19 12:57:18 -0500

    fix(router): retain only request metadata

--- core/kumbia/router.php
+++ core/kumbia/router.php
@@ -104,7 +104,10 @@ class Router
         }
 
         // Descompone la url y conserva los datos de la petición inicial
-        self::$vars = $router::rewrite($url) + self::DEFAULT + self::$vars;
+        self::$vars = $router::rewrite($url) + self::DEFAULT + [
+            'route' => self::$vars['route'],
+            'method' => self::$vars['method'],
+        ];
 
         // Despacha la ruta actual
         return static::dispatch($router::getController(self::$vars));
